db8aedb7d1158f0ea8a48a32d0c48017323e399d
Hadi <hadilashkari@gmail.com>
2020-12-10 21:44:05 +0100

    Initial config

---
+++ home.nix
@@ -0,0 +1,102 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "hadi.lashkari";
+  home.homeDirectory = "/Users/hadi.lashkari";
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.android_sdk.accept_license = true;
+
+  home.packages = with pkgs; [
+    htop
+    screen
+    zsh
+    nix-zsh-completions
+    jetbrains-mono
+    openjdk8
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    initExtra = ''
+      export JAVA_HOME="/Users/hadi.lashkari/jdk"
+      export PATH=$PATH:/usr/local/bin
+
+      source $HOME/.zshrc-credentials
+
+    '';
+
+    shellAliases = {
+      ff = "/Applications/Firefox.app/Contents/MacOS/firefox -P --no-remote&";
+    };
+
+    initExtraBeforeCompInit = ''
+      . /Users/hadi.lashkari/.nix-profile/etc/profile.d/nix.sh
+    '';
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "amuse";
+      plugins = [ "git" "sudo" "docker" "kubectl" ];
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Hadi";
+    userEmail = "hadilashkari@gmail.com";
+  };
+
+  programs.vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-flutter
+      vim-flatbuffers
+      vim-android
+      rust-vim
+      vim-ruby
+    ];
+    settings = { ignorecase = true; };
+    extraConfig = ''
+      syntax on
+      filetype plugin indent on
+      " On pressing tab, insert 2 spaces
+      set expandtab
+      " Show existing tabl with 2 space width
+      set tabstop=2
+      set softtabstop=2
+      " When indent with '>' use 2 spaces width
+      set shiftwidth=2
+      inoremap jj <Esc>
+
+      " set clipboard=unnamed
+    '';
+  };
+
+  home.file = {
+    ".screenrc" = {
+      text = ''
+        defscrollback 5000
+      '';
+    };
+
+    "jdk".source = "${pkgs.openjdk8}";
+    "jdk8".source = "${pkgs.openjdk8}";
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.03";
+}
