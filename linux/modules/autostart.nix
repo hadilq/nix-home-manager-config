d1ca7533776c15d0d67d9eeb8a56f6d3bd465e4f
Hadi <hadilq.dev@gmail.com>
2026-03-04 09:34:22 -0500

    update autostart

--- linux/modules/autostart.nix
+++ linux/modules/autostart.nix
@@ -73,32 +73,6 @@ let
   };
 
   autostart-command = pkgs.writeShellScriptBin "autostart-command" ''
-    #count=1
-    #while ! ping -c 1 -W 1 google.com; do
-    #  # notify-send --icon computer 'Waiting on network'
-    #  sleep 3
-    #  ((count++))
-    #  if [ $count -eq 10 ]; then
-    #      # notify-send --icon computer 'Unable to connect'
-    #      break
-    #  fi
-    #done
-
-    ## Browser
-    #tmux new-session -d -s firefox -n default
-    #tmux new-window -n hadi-pro -t firefox: 'firefox -P "hadi.pro" --no-remote'
-
-    #tmux new-session -d -s librewolf -n default
-    #tmux new-window -n proton -t librewolf: 'librewolf -P "proton" --no-remote'
-    #tmux new-window -n github -t  librewolf: ' librewolf -P "github" --no-remote'
-
-    #tmux new-session -d -s zen-browser -n default
-    #tmux new-window -n ml -t zen-browser: 'zen -P "ML" --no-remote'
-
-    # # Signal
-    # tmux new-session -d -s signal -n default
-    # tmux new-window -n run -t signal: 'signal-desktop'
-
     # home-manager
     tmux new-session -d -s home-manager -n default
     ${lib.optionalString (builtins.hasAttr "home-manager-config-dir" local-projects) "tmux new-window -n nvim -t home-manager: 'cd ${local-projects.home-manager-config-dir} && nvim home.nix'"}
@@ -106,21 +80,14 @@ let
     ${lib.optionalString (builtins.hasAttr "home-manager-dir" local-projects) "tmux new-window -n nvim -t home-manager: 'cd ${local-projects.home-manager-dir} && nvim flake.nix'"}
     ${lib.optionalString (builtins.hasAttr "nixos-config-dir" local-projects) "tmux new-window -n nvim -t home-manager: 'cd ${local-projects.nixos-config-dir} && nvim flake.nix'"}
   '';
-
-  tmux = pkgs.writeTextFile {
-    name = "tmux-desktop";
-    text = ''
-      [Desktop Entry]
-      Type=Application
-      Name=Tmux autostart
-      Exec=bash -c ${autostart-command}/bin/autostart-command
-      Comment=Start Tmux on desktop start
-    '';
-  };
 in
 {
   home.activation.autostart-command = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     mkdir -p /home/${localConfig.userName}/.config/autostart
+    rm -f /home/${localConfig.userName}/.config/autostart/*
+    ln -sfn  $VERBOSE_ARG \
+      ${autostart-command}/bin/autostart-command \
+      /home/${localConfig.userName}/.config/autostart/autostart-command
     ln -sfn  $VERBOSE_ARG \
       ${zen-ml} \
       /home/${localConfig.userName}/.config/autostart/zen-ml.desktop
@@ -139,8 +106,5 @@ in
     ln -sfn  $VERBOSE_ARG \
       ${terminal} \
       /home/${localConfig.userName}/.config/autostart/terminal.desktop
-    ln -sfn  $VERBOSE_ARG \
-      ${tmux} \
-      /home/${localConfig.userName}/.config/autostart/tmux.desktop
   '';
 }
